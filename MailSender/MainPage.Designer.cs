e47c04c0ee82519f8f208050e8cd8de5dde2d9f7
Rakibuz <pranto055@gmail.com>
2018-04-19 22:09:46 +0600

    Add files via upload

---
+++ MailSender/MainPage.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace OutLookMail
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.To_lebel = new System.Windows.Forms.Label();
+            this.Subject_lebel = new System.Windows.Forms.Label();
+            this.To_textbox = new System.Windows.Forms.TextBox();
+            this.Subject_textbox = new System.Windows.Forms.TextBox();
+            this.Message_textbox = new System.Windows.Forms.TextBox();
+            this.Send_button = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Message_lebel = new System.Windows.Forms.Label();
+            this.parse_msg = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // To_lebel
+            // 
+            this.To_lebel.AutoSize = true;
+            this.To_lebel.Location = new System.Drawing.Point(18, 9);
+            this.To_lebel.Name = "To_lebel";
+            this.To_lebel.Size = new System.Drawing.Size(20, 13);
+            this.To_lebel.TabIndex = 0;
+            this.To_lebel.Text = "To";
+            // 
+            // Subject_lebel
+            // 
+            this.Subject_lebel.AutoSize = true;
+            this.Subject_lebel.Location = new System.Drawing.Point(11, 45);
+            this.Subject_lebel.Name = "Subject_lebel";
+            this.Subject_lebel.Size = new System.Drawing.Size(43, 13);
+            this.Subject_lebel.TabIndex = 1;
+            this.Subject_lebel.Text = "Subject";
+            this.Subject_lebel.Click += new System.EventHandler(this.Subject_lebel_Click);
+            // 
+            // To_textbox
+            // 
+            this.To_textbox.Location = new System.Drawing.Point(74, 7);
+            this.To_textbox.Multiline = true;
+            this.To_textbox.Name = "To_textbox";
+            this.To_textbox.Size = new System.Drawing.Size(596, 19);
+            this.To_textbox.TabIndex = 2;
+            // 
+            // Subject_textbox
+            // 
+            this.Subject_textbox.Location = new System.Drawing.Point(74, 45);
+            this.Subject_textbox.Multiline = true;
+            this.Subject_textbox.Name = "Subject_textbox";
+            this.Subject_textbox.Size = new System.Drawing.Size(596, 19);
+            this.Subject_textbox.TabIndex = 3;
+            this.Subject_textbox.TextChanged += new System.EventHandler(this.Subject_textbox_TextChanged);
+            // 
+            // Message_textbox
+            // 
+            this.Message_textbox.Location = new System.Drawing.Point(74, 86);
+            this.Message_textbox.Multiline = true;
+            this.Message_textbox.Name = "Message_textbox";
+            this.Message_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Message_textbox.Size = new System.Drawing.Size(596, 72);
+            this.Message_textbox.TabIndex = 4;
+            // 
+            // Send_button
+            // 
+            this.Send_button.Location = new System.Drawing.Point(595, 156);
+            this.Send_button.Name = "Send_button";
+            this.Send_button.Size = new System.Drawing.Size(75, 23);
+            this.Send_button.TabIndex = 6;
+            this.Send_button.Text = "Send";
+            this.Send_button.UseVisualStyleBackColor = true;
+            this.Send_button.Click += new System.EventHandler(this.Send_button_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(674, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(174, 382);
+            this.webBrowser.TabIndex = 7;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Subject,
+            this.Sender,
+            this.DateTime});
+            this.dataGridView.Location = new System.Drawing.Point(14, 185);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(347, 207);
+            this.dataGridView.TabIndex = 8;
+            this.dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellClick);
+            // 
+            // Subject
+            // 
+            this.Subject.DataPropertyName = "Subject";
+            this.Subject.HeaderText = "Subject";
+            this.Subject.Name = "Subject";
+            // 
+            // Sender
+            // 
+            this.Sender.DataPropertyName = "Sender";
+            this.Sender.HeaderText = "Sender";
+            this.Sender.Name = "Sender";
+            // 
+            // DateTime
+            // 
+            this.DateTime.DataPropertyName = "Date";
+            this.DateTime.HeaderText = "DateTime";
+            this.DateTime.Name = "DateTime";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(595, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Receive";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 400);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(555, 12);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // Message_lebel
+            // 
+            this.Message_lebel.AutoSize = true;
+            this.Message_lebel.Location = new System.Drawing.Point(11, 86);
+            this.Message_lebel.Name = "Message_lebel";
+            this.Message_lebel.Size = new System.Drawing.Size(50, 13);
+            this.Message_lebel.TabIndex = 5;
+            this.Message_lebel.Text = "Message";
+            // 
+            // parse_msg
+            // 
+            this.parse_msg.Location = new System.Drawing.Point(367, 185);
+            this.parse_msg.Multiline = true;
+            this.parse_msg.Name = "parse_msg";
+            this.parse_msg.Size = new System.Drawing.Size(301, 207);
+            this.parse_msg.TabIndex = 11;
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 433);
+            this.Controls.Add(this.parse_msg);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.Send_button);
+            this.Controls.Add(this.Message_lebel);
+            this.Controls.Add(this.Message_textbox);
+            this.Controls.Add(this.Subject_textbox);
+            this.Controls.Add(this.To_textbox);
+            this.Controls.Add(this.Subject_lebel);
+            this.Controls.Add(this.To_lebel);
+            this.MaximizeBox = false;
+            this.Name = "MainPage";
+            this.Text = "OutLookMail";
+            this.Load += new System.EventHandler(this.MainPage_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label To_lebel;
+        private System.Windows.Forms.Label Subject_lebel;
+        private System.Windows.Forms.TextBox To_textbox;
+        private System.Windows.Forms.TextBox Subject_textbox;
+        private System.Windows.Forms.TextBox Message_textbox;
+        private System.Windows.Forms.Button Send_button;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label Message_lebel;
+        private System.Windows.Forms.TextBox parse_msg;
+    }
+}
+
---
+++ MailSender/MainPage.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OutLook = Microsoft.Office.Interop.Outlook;
+using System.Threading;
+using System.Configuration;
+using Microsoft.CognitiveServices.SpeechRecognition;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Media;
+using System.Windows;
+using System.Text.RegularExpressions;
+
+namespace OutLookMail
+{
+    public partial class MainPage : Form
+    {
+        AutoResetEvent _FinalResponceEvent;
+        MicrophoneRecognitionClient _microphoneRecognitionClient;
+
+
+        //Variable decleration for  MSAPI
+        SpeechSynthesizer speechsynth = new SpeechSynthesizer();
+        SpeechRecognitionEngine receng = new SpeechRecognitionEngine();
+        Choices choice = new Choices();
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _FinalResponceEvent = new AutoResetEvent(false);
+
+
+        }
+
+        private void ConvertSpeechToText()
+        {
+            // Converting speech method for Subject text box
+            var speechRecognitionMode = SpeechRecognitionMode.ShortPhrase;
+            string language = "en-us";
+            string subscriptionKey = ConfigurationManager.AppSettings["MicrosoftSpeechApiKey"].ToString();
+            _microphoneRecognitionClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                speechRecognitionMode,
+                language,
+                subscriptionKey
+                 );
+            _microphoneRecognitionClient.OnPartialResponseReceived += ResponseReceived;
+            _microphoneRecognitionClient.OnResponseReceived += OnMicShortPhraseResponceReceivedHandler;
+            _microphoneRecognitionClient.StartMicAndRecognition();
+        }
+
+        private void ConvertSpeechToTextnew()
+        {
+            // Converting speech method for Message_textbox
+            var speechRecognitionMode = SpeechRecognitionMode.ShortPhrase;
+            string language = "en-us";
+            string subscriptionKey = ConfigurationManager.AppSettings["MicrosoftSpeechApiKey"].ToString();
+            _microphoneRecognitionClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                speechRecognitionMode,
+                language,
+                subscriptionKey
+                 );
+            _microphoneRecognitionClient.OnPartialResponseReceived += ResponseReceivednew;
+            _microphoneRecognitionClient.OnResponseReceived += OnMicShortPhraseResponceReceivedHandlernew;
+            _microphoneRecognitionClient.StartMicAndRecognition();
+        }
+
+        private void OnMicShortPhraseResponceReceivedHandlernew(object sender, SpeechResponseEventArgs e)
+        {
+            //Message textbox Eesponsed received invoke
+            Invoke((Action)(() =>
+            {
+                Message_textbox.BackColor = Color.White;
+                Message_textbox.ForeColor = Color.Black;
+            }));
+        }
+
+        private void ResponseReceivednew(object sender, PartialSpeechResponseEventArgs e)
+        {
+            //Message textbox Eesponsed received invoke
+            string result = e.PartialResult;
+
+            Message_textbox.BeginInvoke((Action)(() =>
+
+            Message_textbox.Text = result));
+
+        }
+
+        private void OnMicShortPhraseResponceReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+            //Subject textbox Eesponsed received invoke
+            //btn_record.Enabled = true;
+            //textBox1.BackColor = Color.White;
+            //textBox1.ForeColor = Color.Black;
+            Invoke((Action)(() =>
+            {
+                Subject_textbox.BackColor = Color.White;
+                Subject_textbox.ForeColor = Color.Black;
+            }));
+        }
+
+        private void ResponseReceived(object sender, PartialSpeechResponseEventArgs e)
+        {
+            // Subject textbox Eesponsed received invoke
+            string result = e.PartialResult;
+
+            Subject_textbox.BeginInvoke((Action)(() =>
+
+            Subject_textbox.Text = result));
+
+
+
+        }
+
+        private void MainPage_Load_1(object sender, EventArgs e)
+        {
+
+            //nothing will be done here
+            choice.Add(new string[] { "hello", "Subject", "write body", "send mail", "Load Inbox", "wake up", "sleep mode", "Close OutlookMail", "Close Application", "Read Mail", "Append receiver" });
+            Grammar gr = new Grammar(new GrammarBuilder(choice));
+            try
+            {
+                receng.RequestRecognizerUpdate();
+                receng.LoadGrammar(gr);
+                receng.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(receng_SpeechRecognized);
+                receng.AudioLevelUpdated += new EventHandler<AudioLevelUpdatedEventArgs>(receng_AudioLevelUpdated);
+                receng.SetInputToDefaultAudioDevice();
+                receng.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+        }
+        private void receng_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            progressBar1.Value = e.AudioLevel;
+
+        }
+
+        private void receng_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String Speech = e.Result.Text;
+
+
+            if (Speech == "wake up") global.wake = true;
+            if (Speech == "sleep mode") global.wake = false;
+
+
+            if (global.wake == true)
+            {
+                
+                //throw new NotImplementedException();
+                switch (Speech)
+                {
+                    case "hello":
+                        speechsynth.SelectVoiceByHints(VoiceGender.Female);
+                        speechsynth.SpeakAsync("hey master what can i do for you Now");
+                        break;
+                    case "Append receiver":
+                        speechsynth.SpeakAsync("ok alright");
+                        string address = "pranto055@gmail.com";
+                        To_textbox.Text += address; //appending the string to To_textbox
+
+                        break;
+                    case "Subject":
+                        Subject_textbox.Focus();
+                        Subject_textbox.BackColor = Color.Green;
+                        Subject_textbox.ForeColor = Color.White;
+                        ConvertSpeechToText(); // calling method for subject textbox
+                        break;
+                    case "write body":
+                        Message_textbox.Focus();
+                        Message_textbox.BackColor = Color.Green;
+                        Message_textbox.ForeColor = Color.White;
+                        ConvertSpeechToTextnew();//calling method for Message text box
+                        break;
+                    case "send mail":
+                        global.wake = false;
+                        speechsynth.SpeakAsync("Alright you said to sending mail");
+                        Send_button.PerformClick();
+                        //speechsynth.SpeakAsync("sending mail be patient please");
+                        break;
+                    case "Load Inbox":
+                        global.wake = false;
+                        speechsynth.SpeakAsync("Give me a few second i am working over it");
+                        button1.PerformClick();
+                        break;
+                    case "Close OutlookMail":
+                        speechsynth.SpeakAsyncCancelAll();
+                        this.Close();
+                        speechsynth.SpeakAsync("Ok sir");
+                        break;
+                    case "Close Application":
+                        speechsynth.SpeakAsyncCancelAll();
+                        this.Close();
+                        speechsynth.SpeakAsync("Ok sir");
+                        break;
+                    case "Read Mail":
+                        speechsynth.SpeakAsync("");
+
+                        readmessage();
+
+                        break;
+
+                }
+
+            }
+
+        }
+
+        private void readmessage()
+        {
+            global.wake = false;//turn on sleep mode to prevent garbage input at reading time
+
+            button1.PerformClick();//load inbox from gemail
+             //dataGridView.Rows[1].Selected = true;
+            dataGridView.Rows[dataGridView.Rows.Count - 1].Selected = true;
+
+            webBrowser.DocumentText = dt.Rows[dataGridView.Rows.Count - 1]["Body"].ToString();
+
+            Int32 index = dataGridView.Rows.Count - 1;
+
+
+            speechsynth.SpeakAsync("Sender");
+            speechsynth.SpeakAsync(dt.Rows[index]["Sender"].ToString());
+
+            speechsynth.SpeakAsync("Subject");
+            speechsynth.SpeakAsync(dt.Rows[index]["Subject"].ToString());
+
+             
+            speechsynth.SpeakAsync("Date and time");
+            // dt.Rows[index]["Date"].ToString().Trim();
+            
+            speechsynth.SpeakAsync(dt.Rows[index]["Date"].ToString());
+
+            parse_msg.Text = dt.Rows[index]["Body"].ToString();
+            parse_msg.Text = parse_msg.Text.Replace("<br />", Environment.NewLine);
+            parse_msg.Text = Regex.Replace(parse_msg.Text, "<.*?>", String.Empty);
+            speechsynth.SpeakAsync("Mail Body");
+            speechsynth.SpeakAsync(parse_msg.Text);
+
+            dt.Rows[index]["Date"].ToString().Trim();
+
+
+            //string Value = dataGridView.Rows[0].Cells[0].Value.ToString();
+            //speechsynth.SpeakAsync("Sender");
+            //speechsynth.SpeakAsync(Value);
+
+            //string Date = dataGridView.Rows[0].Cells[3].Value.ToString();
+            //speechsynth.SpeakAsync("Date and time");
+            //speechsynth.SpeakAsync(Date);
+
+
+
+
+        }
+
+        public void readmessage(object sender, DataGridViewCellEventArgs e)
+        {
+            button1.PerformClick();//load inbox from gemail
+         
+            //callcellclick
+            //dataGridView.Rows[dataGridView.Rows.Count - 1].Selected = true;
+            // dataGridView.Rows[1].Selected = true;
+
+            // dataGridView.Rows[0].
+            dataGridView_CellClick(sender, e);
+
+        }
+
+       
+        // here angelkona077@gmail.com in logedin in outlook rsp055@gmail.com 
+        // sender use ase pranto055@gmail.com
+        private void Send_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OutLook._Application _app = new OutLook.Application();
+                OutLook.MailItem mail = (OutLook.MailItem)_app.CreateItem(OutLook.OlItemType.olMailItem);
+                mail.To = To_textbox.Text;
+                mail.Subject = Subject_textbox.Text;
+                mail.Body = Message_textbox.Text;
+                mail.Importance = OutLook.OlImportance.olImportanceNormal;
+                ((OutLook.MailItem)mail).Send();
+                // MessageBox.Show("Your Message hasbeen successfully sent.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                speechsynth.SpeakAsync("your mail sending has been successfully done ");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                speechsynth.SpeakAsync("Sorry sir i failed to sending mail check the email address please");
+            }
+        }
+
+        DataTable dt;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OutLook.Application _app = new OutLook.Application();
+                OutLook.NameSpace _ns = _app.GetNamespace("MAPI");
+                OutLook.MAPIFolder inbox = _ns.GetDefaultFolder(OutLook.OlDefaultFolders.olFolderInbox);
+                _ns.SendAndReceive(true);
+                dt = new DataTable("Inbox");
+                dt.Columns.Add("Subject", typeof(String));
+                dt.Columns.Add("Sender", typeof(String));
+                dt.Columns.Add("Body", typeof(String));
+                dt.Columns.Add("Date", typeof(String));
+
+                dataGridView.DataSource = dt;
+
+                foreach (OutLook.MailItem item in inbox.Items)
+                    dt.Rows.Add(new object[] { item.Subject, item.SenderName, item.HTMLBody, item.SentOn.ToLongDateString() + "" + item.SentOn.ToLongTimeString() });
+                 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < dt.Rows.Count && e.RowIndex >= 0)
+                webBrowser.DocumentText = dt.Rows[e.RowIndex]["Body"].ToString();//showing parsed content in web browser
+
+                speechsynth.SpeakAsync("Sender");
+                speechsynth.SpeakAsync(dt.Rows[e.RowIndex]["Sender"].ToString());
+                speechsynth.SpeakAsync("Sending time");
+                speechsynth.SpeakAsync(dt.Rows[e.RowIndex]["Date"].ToString());
+
+            speechsynth.SpeakAsync("Message is");
+            parse_msg.Text = dt.Rows[e.RowIndex]["Body"].ToString();
+            parse_msg.Text = parse_msg.Text.Replace("<br />", Environment.NewLine);
+            parse_msg.Text = Regex.Replace(parse_msg.Text, "<.*?>", String.Empty);
+            speechsynth.SpeakAsync(parse_msg.Text);
+
+
+        }
+
+        private void Subject_lebel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Subject_textbox_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
